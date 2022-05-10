460f4675f84191d530fa2b01994dd57ff0a72e5b
barry <barry@netlands.com>
2022-05-10 09:43:35 +0900

    Added shortcuts

---
+++ start.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\Program Files (x86)\Microsoft\Edge\Application\msedge_proxy.exe"  --profile-directory=Default --app-id=hbblfifohofgngfbjbiimbbcimepbdcb --app-url=http://localhost:3000/ --app-launch-source=4
+node app
+
+
