1af4efa914a3c0c92870c19efee74ae0a23192fd
barry <barry@netlands.com>
2022-05-26 11:14:33 +0900

    Build updates

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-npx electron-packager . --overwrite --asar --ignore=^/art --ignore=^/copy --ignore=^/original --ignore=^/copy --icon='./camera.ico'
-
+@echo off
+call npx electron-packager . --overwrite --asar --ignore=^/art --ignore=^/copy --ignore=^/original --ignore=^/copy --icon='./camera.ico'
+xcopy ".\config.local.json" ".\Simple Photo Sync-win32-x64\config.local.json*"
+TIMEOUT /T 5
