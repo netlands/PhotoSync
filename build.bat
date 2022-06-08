0ec55121f4a908d4f8dcc287d459c0ca2ce7ad1f
barry <barry@netlands.com>
2022-06-08 17:25:13 +0900

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,4 +1,7 @@
 @echo off
 call npx electron-packager . --overwrite --asar --out=./dist/ --ignore=^/art --ignore=^/copy --ignore=^/original --ignore=^/copy --icon='./SimplePhotoSync.ico'
-REM xcopy ".\config.local.json" ".\dist\Simple Photo Sync-win32-x64\config.local.json*"
+powershell Compress-Archive -Path '.\dist\Simple Photo Sync-win32-x64\*' -DestinationPath '.\dist\Simple Photo Sync-win32-x64.zip'
+REM files for local testing
+xcopy ".\config.local.json" ".\dist\Simple Photo Sync-win32-x64\config.local.json*"
+xcopy ".\reference.JPG" ".\dist\Simple Photo Sync-win32-x64\reference.JPG*"
 TIMEOUT /T 5
