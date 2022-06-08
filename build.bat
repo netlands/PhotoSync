86987c70d23b108c54f599400879c3c433a035a6
barry <barry@netlands.com>
2022-06-09 08:13:36 +0900

    Update for local testing

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 call npx electron-packager . --overwrite --asar --out=./dist/ --ignore=^/art --ignore=^/copy --ignore=^/original --ignore=^/copy --icon='./SimplePhotoSync.ico'
+del .\dist\*.zip
 powershell Compress-Archive -Path '.\dist\*' -DestinationPath '.\dist\Simple Photo Sync-win32-x64.zip' -Force
 REM files for local testing
 xcopy ".\config.local.json" ".\dist\Simple Photo Sync-win32-x64\config.local.json*"
